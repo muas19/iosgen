3a314aa202dce9bf43ccb490015b0ea92ce22065
back2back <muasyaleon@gmail.com>
2024-11-08 08:02:49 +0300

    Initial Commit

---
+++ iosgen/iosgenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosgenApp.swift
+//  iosgen
+//
+//  Created by leon muasya on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct iosgenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
